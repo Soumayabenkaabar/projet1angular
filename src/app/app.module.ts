3b07fb05a3d0da3798e8e819c176bb0b4a7bb1cd
soumaya <soumayabenkaabar4@gmail.com>
2024-11-03 11:15:17 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';// nest7a9oh f ng model 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { ExerciceComponent } from './exercice/exercice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    ExerciceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/exercice/exercice.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice',
+  templateUrl: './exercice.component.html',
+  styleUrl: './exercice.component.css'
+})
+export class ExerciceComponent {
+name: any;
+nom: any;
+age: any;
+a:any= 10;
+b:any= 20;
+img= "img1.png";
+s=0;
+
+afficher():void {
+  this.s=this.a+this.b;
+  alert("la somme de "+this.a+" et "+this.b+" = "+this.s);
+}
+effacer():void {
+this.nom="";  
+this.age="";
+}
+}
